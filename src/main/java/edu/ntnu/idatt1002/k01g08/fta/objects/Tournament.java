b51752250b78464b70eca309828d6bfdc13f83df
flippi321 <90700334+flippi321@users.noreply.github.com>
2022-03-21 12:40:58 +0100

    Expanded Abstract Tournament class

--- src/main/java/edu/ntnu/idatt1002/k01g08/fta/objects/Tournament.java
+++ src/main/java/edu/ntnu/idatt1002/k01g08/fta/objects/Tournament.java
@@ -1,5 +1,36 @@
 package edu.ntnu.idatt1002.k01g08.fta.objects;
+import java.util.ArrayList;
 
 public abstract class Tournament {
+    ArrayList<Team> teams;
+    private String tournamentName;
+    private int firstPrize;
+    private String startDate;
 
+    /**
+     * Constructor for object which represents an abstract tournament
+     * @param teams a list of all teams in the tournament
+     * @param tournamentName the name of the tournament
+     * @param firstPrize what you get for achieving first place
+     * @param startDate when the tournament starts
+     */
+    public Tournament(ArrayList<Team> teams, String tournamentName, int firstPrize, String startDate) {
+        this.teams = teams;
+        this.tournamentName = tournamentName;
+        this.firstPrize = firstPrize;
+        this.startDate = startDate;
+    }
+
+    /**
+     * Constructor for object which represents an abstract tournament, without a winning prize
+     * @param teams a list of all teams in the tournament
+     * @param tournamentName the name of the tournament
+     * @param startDate when the tournament starts
+     */
+    public Tournament(ArrayList<Team> teams, String tournamentName, String startDate) {
+        this.teams = teams;
+        this.tournamentName = tournamentName;
+        this.startDate = startDate;
+        this.firstPrize = 0;
+    }
 }
