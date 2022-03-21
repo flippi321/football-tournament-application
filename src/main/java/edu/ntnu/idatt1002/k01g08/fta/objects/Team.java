9169fde3876699f8adc06a1aa6658e023717de84
Bendik Madsø Engen <bendikme@stud.ntnu.no>
2022-03-21 12:35:55 +0100

    Started work on Team class.

--- src/main/java/edu/ntnu/idatt1002/k01g08/fta/objects/Team.java
+++ src/main/java/edu/ntnu/idatt1002/k01g08/fta/objects/Team.java
@@ -1,4 +1,54 @@
 package edu.ntnu.idatt1002.k01g08.fta.objects;
 
-public class Team {
+import java.util.*;
+
+public class Team implements Iterable<Player> {
+    final private String name;
+    final private Map<Integer, Player> players;
+
+    public Team(String name) {
+        this.name = name;
+        this.players = new HashMap<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Player getPlayer(int number) {
+        return players.get(number);
+    }
+
+    public boolean addPlayer(Player player) {
+        if (player == null || players.containsKey(player.getNumber())) {
+            return false;
+        }
+        players.put(player.getNumber(), player);
+        return true;
+    }
+
+    public boolean removePlayer(Player player) {
+        return players.remove(player.getNumber(), player);
+    }
+
+    public boolean removePlayer(int number) {
+        return (players.remove(number) != null);
+    }
+
+    @Override
+    public Iterator<Player> iterator() {
+        return new Iterator<>() {
+            final Iterator<Integer> keyIt = players.keySet().iterator();
+
+            @Override
+            public boolean hasNext() {
+                return keyIt.hasNext();
+            }
+
+            @Override
+            public Player next() {
+                return getPlayer(keyIt.next());
+            }
+        };
+    }
 }
