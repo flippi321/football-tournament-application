3e743158a3350d0afaf3acdb4f699dd42956b46c
Bendik Madsø Engen <bendikme@stud.ntnu.no>
2022-03-21 11:34:15 +0100

    Created PlayerTest class.

---
+++ src/test/java/objects/PlayerTest.java
@@ -0,0 +1,46 @@
+package objects;
+
+import edu.ntnu.idatt1002.k01g08.fta.objects.Player;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PlayerTest {
+    @Test
+    public void statisticsStartAtZero() {
+        Player player = new Player("Ole Gunnar Solskjær", 40);
+
+        assertEquals(0, player.getAssists());
+        assertEquals(0, player.getGoals());
+        assertEquals(0, player.getRedCards());
+        assertEquals(0, player.getYellowCards());
+    }
+
+    @Test
+    public void statisticsIncrement() {
+        Player player = new Player("Ole Gunnar Solskjær", 40);
+
+        player.increaseYellowCards();
+        player.increaseYellowCards();
+        player.increaseYellowCards();
+        player.increaseYellowCards();
+        player.increaseRedCards();
+        player.increaseRedCards();
+        player.increaseRedCards();
+        player.increaseGoals();
+        player.increaseGoals();
+        player.increaseAssists();
+
+        assertEquals(1, player.getAssists());
+        assertEquals(2, player.getGoals());
+        assertEquals(3, player.getRedCards());
+        assertEquals(4, player.getYellowCards());
+    }
+
+    @Test
+    public void nameAndNumberAreCorrect() {
+        Player player = new Player("Ole Gunnar Solskjær", 40);
+
+        assertEquals("Ole Gunnar Solskjær", player.getName());
+        assertEquals(40, player.getNumber());
+    }
+}
