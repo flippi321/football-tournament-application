2dfa65629f36ce1500d54a8e105eead3998a9ff5
Bendik Madsø Engen <bendikme@stud.ntnu.no>
2022-03-23 08:13:09 +0100

    Started work on Match class.

--- src/main/java/edu/ntnu/idatt1002/k01g08/fta/objects/Match.java
+++ src/main/java/edu/ntnu/idatt1002/k01g08/fta/objects/Match.java
@@ -1,4 +1,38 @@
 package edu.ntnu.idatt1002.k01g08.fta.objects;
 
-public class Match {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Match implements Iterable<GameEvent> {
+    final private Team homeTeam;
+    final private Team awayTeam;
+    final private List<GameEvent> matchHistory;
+
+    public Match(Team homeTeam, Team awayTeam) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.matchHistory = new ArrayList<>();
+    }
+
+    public Team getHomeTeam() {
+        return homeTeam;
+    }
+
+    public Team getAwayTeam() {
+        return awayTeam;
+    }
+
+    public GameEvent getGameEvent(int i) {
+        return matchHistory.get(i);
+    }
+
+    @Override
+    public Iterator<GameEvent> iterator() {
+        return matchHistory.iterator();
+    }
+
+    public void addGameEvent(GameEvent gameEvent) {
+        matchHistory.add(gameEvent);
+    }
 }
