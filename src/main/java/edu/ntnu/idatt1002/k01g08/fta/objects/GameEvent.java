4a3fbbb917c8458145533b05dcf207b90d18e3c4
Bjørn-Johnny Bendixen <bjornjob@stud.ntnu.no>
2022-03-23 08:25:21 +0100

    Created class GameEvent.

--- src/main/java/edu/ntnu/idatt1002/k01g08/fta/objects/GameEvent.java
+++ src/main/java/edu/ntnu/idatt1002/k01g08/fta/objects/GameEvent.java
@@ -1,4 +1,21 @@
 package edu.ntnu.idatt1002.k01g08.fta.objects;
 
-public class GameEvent {
+public abstract class GameEvent {
+    private Player player;
+    private Team team;
+
+    public GameEvent(Player player, Team team) {
+        this.player = player;
+        this.team = team;
+    }
+
+    public abstract int getAction();
+
+    @Override
+    public String toString() {
+        return "GameEvent{" +
+                "player=" + player +
+                ", team=" + team +
+                '}';
+    }
 }
