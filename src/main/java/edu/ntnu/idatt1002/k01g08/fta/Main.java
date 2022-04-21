fe2d9aa3b6e77549362f6ba0a7d1582dcb350ea1
johnfb <johnfb@stud.ntnu.no>
2022-04-21 11:45:20 +0200

    Fixed typo

--- src/main/java/edu/ntnu/idatt1002/k01g08/fta/Main.java
+++ src/main/java/edu/ntnu/idatt1002/k01g08/fta/Main.java
@@ -7,7 +7,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -21,8 +20,6 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try{
-            //SceneManager.setView("hello-view");
-
             FXMLLoader loader = SceneManager.getLoader("main");
             SceneManager.setScene(new Scene(loader.load()));
             SceneManager.setCurrentScene("main");
