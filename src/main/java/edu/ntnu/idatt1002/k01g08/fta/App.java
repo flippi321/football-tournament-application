bf282967d106beea3fbccccb24c90690580dba98
johnfb <johnfb@stud.ntnu.no>
2022-04-27 16:37:48 +0200

    Build fix

---
+++ src/main/java/edu/ntnu/idatt1002/k01g08/fta/App.java
@@ -0,0 +1,10 @@
+package edu.ntnu.idatt1002.k01g08.fta;
+
+/**
+ * Class used to run the entire application
+ */
+public class App {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
