5a605149481f3647a1188cf991429db54e96dd47
johnfb <johnfb@stud.ntnu.no>
2022-04-28 17:12:00 +0200

    Fixed compile-error

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,8 +9,6 @@ module edu.ntnu.idatt1002.k01g08.fta {
     exports edu.ntnu.idatt1002.k01g08.fta;
     exports edu.ntnu.idatt1002.k01g08.fta.registers;
     opens edu.ntnu.idatt1002.k01g08.fta.registers to javafx.fxml;
-    exports edu.ntnu.idatt1002.k01g08.fta.objects;
-    opens edu.ntnu.idatt1002.k01g08.fta.objects to javafx.fxml;
     exports edu.ntnu.idatt1002.k01g08.fta.controllers;
     opens edu.ntnu.idatt1002.k01g08.fta.controllers to javafx.fxml;
     exports edu.ntnu.idatt1002.k01g08.fta.guiControllers;
